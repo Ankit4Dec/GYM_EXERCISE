064ac481f6939f9b3363941d4c6d4143ef362644
Ankit <ankit27866@gmail.com>
2024-07-15 20:51:48 +0530

    Added Pages and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Home } from "@mui/icons-material";
+import ExerciseDetails from "./pages/ExerciseDetails";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/exercise/id:" element={<ExerciseDetails />}></Route>
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
