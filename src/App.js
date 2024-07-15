cb7a6900e2812c520217c63c84ef5532a9722db1
Ankit <ankit27866@gmail.com>
2024-07-15 21:08:12 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { Home } from "@mui/icons-material";
 import ExerciseDetails from "./pages/ExerciseDetails";
+import Home from "./pages/Home";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
